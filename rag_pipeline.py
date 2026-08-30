0cd1ba2b55db211834df5c3f0b1d60ce92c989e7
GeorgeSinfield <george.sinfield.cs@gmail.com>
2026-08-30 04:29:07 +0100

    added chunk_text function

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -22,5 +22,33 @@ def load_pdf (filepath):
 #Run function on pdf
 text = load_pdf("goldman_bdc_10k.pdf")
 
-#Print function's first 500 words
-print(text[:500])
+#Function to chunk the text
+def chunk_text(text, chunk_size=200, overlap=20):
+
+    #Split text into words
+    words = text.split()
+
+    #Total number of words
+    total_words = len(words)
+
+    #Create list where chunks can be stored
+    chunks = []
+
+    #Create step varible
+    step = chunk_size - overlap
+
+    #Create start varible
+    start = 0
+
+    #Loop though words adding them to a chunk which will then be stoerd in chiunks
+    while start < total_words:
+        chunks.append(" ".join(words[start:start + chunk_size]))
+        start += step
+
+    #Retun chunks
+    return chunks
+
+#Run chunk_text on goldman pdf text and print total chunks and the first chunk
+chunks = chunk_text(text)
+print(len(chunks))
+print(chunks[0])
