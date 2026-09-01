6ba2ee32fef3944cb9b592e79da21d690595b722
GeorgeSinfield <george.sinfield.cs@gmail.com>
2026-09-01 11:39:48 +0100

    testing

--- portfolio_risk_engine.py
+++ portfolio_risk_engine.py
@@ -86,8 +86,8 @@ def run_risk_analysis(tickers: list[str], weights: list[float]):
     }
 
 #Test
-result = run_risk_analysis(["AAPL", "JPM", "XOM"], [0.4, 0.3, 0.3])
-print("Volatility:\n", result["volatility"])
-print("\nCorrelation:\n", result["correlation_matrix"])
-print("\nVaR:\n", result["portfolio_var"])
-print("\nMax Drawdown:\n", result["max_drawdown"])
+#result = run_risk_analysis(["AAPL", "JPM", "XOM"], [0.4, 0.3, 0.3])
+#print("Volatility:\n", result["volatility"])
+#print("\nCorrelation:\n", result["correlation_matrix"])
+#print("\nVaR:\n", result["portfolio_var"])
+#print("\nMax Drawdown:\n", result["max_drawdown"])
