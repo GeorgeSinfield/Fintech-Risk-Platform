8591fa295c85fc48185997d6ee9e289a47258e11
GeorgeSinfield <george.sinfield.cs@gmail.com>
2026-09-02 07:46:54 +0100

    add get_news function

--- portfolio_news_agent.py
+++ portfolio_news_agent.py
@@ -2,7 +2,31 @@ import yfinance as yf
 import anthropic
 from dotenv import load_dotenv
 
+#load .env
 load_dotenv()
+
+#cerate anthropic client
 client = anthropic.Anthropic()
 
 
+#Function that gets news about a ticker
+def get_news(ticker):
+
+    #Creates a yf ticker object
+    ticker = yf.Ticker(ticker)
+
+    #Gets the news of the ticker 
+    news = ticker.news
+
+    news = news[0:5]
+    clean_news = []
+
+    for i in news:
+        title = i['content']['title']
+        summary = i['content']['summary']
+        result = title + " - "+ summary
+        clean_news.append(result)
+    
+    return clean_news
+
+print(get_news("AAPL"))
